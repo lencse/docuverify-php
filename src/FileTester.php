977cbe924482dd58c16fe090c31a1e475ca2cdca
Levente Löki <leventeloki@gmail.com>
2019-03-24 01:54:33 +0100

    :hankey: First version

--- src/FileTester.php
+++ src/FileTester.php
@@ -2,10 +2,37 @@
 
 namespace Lencse\Docuverify;
 
+use League\CommonMark\CommonMarkConverter;
+use PHPHtmlParser\Dom;
+
 final class FileTester
 {
     public function testFile(string $path): bool
     {
+        $tmpdir = sys_get_temp_dir() . '/' . uniqid('', true);
+        $converter = new CommonMarkConverter();
+        $html = $converter->convertToHtml(file_get_contents($path));
+        $dom = new Dom();
+        $dom->load($html, [
+            'removeDoubleSpace' => false,
+            'preserveLineBreaks' => true,
+        ]);
+        $codeFragments = $dom->find('code.language-php');
+        shell_exec('rm -rf ' . $tmpdir);
+        shell_exec('mkdir ' . $tmpdir);
+        $autoladDir = realpath(__DIR__ . '/../vendor/autoload.php');
+        $header = <<<EOF
+<?php
+namespace Readme;
+require_once '$autoladDir';
+EOF;
+        foreach ($codeFragments as $i => $codeFragment) {
+            /** @var Dom\HtmlNode $codeFragment */
+            $php = \htmlspecialchars_decode($codeFragment->text());
+            $file = $tmpdir . '/file' . $i . '.php';
+            file_put_contents($file, $header . $php);
+            $x = shell_exec('php ' . $file);
+        }
         return true;
     }
 }
--- test/VerifyTest.php
+++ test/VerifyTest.php
@@ -12,4 +12,28 @@ class VerifyTest extends TestCase
         $tester = new FileTester();
         $this->assertTrue($tester->testFile(__DIR__ . '/fixtures/01.md'));
     }
+
+    public function testFileWithoutPhpCode(): void
+    {
+        $tester = new FileTester();
+        $this->assertTrue($tester->testFile(__DIR__ . '/fixtures/02.md'));
+    }
+
+    public function testFileWithGoodPhpCode(): void
+    {
+        $tester = new FileTester();
+        $this->assertTrue($tester->testFile(__DIR__ . '/fixtures/03.md'));
+    }
+
+    public function testFileWithBadPhpCode(): void
+    {
+        $tester = new FileTester();
+        try {
+            $tester->testFile(__DIR__ . '/fixtures/04.md');
+        } catch (\Throwable $e) {
+            $this->assertTrue(true);
+            return;
+        }
+        $this->assertTrue(true);
+    }
 }
